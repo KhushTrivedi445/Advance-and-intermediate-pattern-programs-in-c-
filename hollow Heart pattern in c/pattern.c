cfd3a2182ed38fab509052d905b0ced2c8385fcc
Khush Trivedi <168230450+KhushTrivedi445@users.noreply.github.com>
2024-06-05 23:43:33 +0530

    Add files via upload

---
+++ hollow Heart pattern in c/pattern.c
@@ -0,0 +1,76 @@
+#include<stdio.h>
+int main () {
+int n;
+printf("Enter the number of rows you want:");
+scanf("%d",&n);
+
+printf("\n");
+//This two loops are for the upper to triangles in heart
+for(int i=2;i<=n;i++){                         
+    for(int k=1;k<=n-i;k++){                    
+
+        printf(" ");
+    }
+    for (int j=1;j<=i;j++){
+        if(j==1|| j==i){
+        printf("* ");
+    } else {
+        printf("  ");
+    }
+    }
+
+    
+       for(int k=1;k<=n-i;k++){
+        printf(" ");
+    }    
+
+
+
+
+    for(int k=1;k<=n-i;k++){
+        printf(" ");
+    } 
+    for(int j=1;j<=i;j++){
+        if(j==1 || j==i){
+        printf("* ");
+    } else {
+        printf("  ");
+    }
+    }
+    printf("\n");
+}
+ // This loop is for bottom reverse triangle
+ for(int i=2;i<=2*n;i++){
+    for(int k=0;k<i-1;k++){
+        printf(" ");
+    }
+    for(int j=2*n;j>=i;j--){
+        if(j==2*n || j==i){
+        printf("* ");
+    } else {
+        printf("  ");
+    } 
+    }
+    printf("\n");
+}
+return 0;
+}
+
+
+/* output :
+ Enter the number of rows you want:5
+
+   * *       * *    
+  *   *     *   *   
+ *     *   *     *  
+*       * *       * 
+ *               *  
+  *             *   
+   *           *    
+    *         *     
+     *       *      
+      *     *       
+       *   * 
+        * *  
+         *   
+*/
