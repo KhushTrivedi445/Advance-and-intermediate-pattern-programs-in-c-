b03f2f46ce850a369a477a971a4d8bec7d983c4e
Khush Trivedi <168230450+KhushTrivedi445@users.noreply.github.com>
2024-06-06 21:52:44 +0530

    Add files via upload

---
+++ rectangle pattern/pattern.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main () {
+    int n,m;
+    printf("Enter the rows of rectangle : ");
+    scanf("%d",&n);
+    printf("Enter the number of columns :");
+    scanf("%d",&m);
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            printf("* ");
+        } 
+        printf("\n");
+    }
+    return 0;
+}
+
+/*
+output :
+Enter the rows of rectangle : 5
+Enter the number of columns :10
+* * * * * * * * * * 
+* * * * * * * * * * 
+* * * * * * * * * * 
+* * * * * * * * * * 
+* * * * * * * * * * 
+
+*/
