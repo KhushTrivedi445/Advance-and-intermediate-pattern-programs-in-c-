389b5e49b8522f11d3a30753f3b94e7587adbec0
Khush Trivedi <168230450+KhushTrivedi445@users.noreply.github.com>
2024-06-07 23:22:39 +0530

    Add files via upload

---
+++ hollow Butterfly wings pattern/pattern.c
@@ -0,0 +1,91 @@
+#include<stdio.h>
+int main () {
+    int n;
+    printf("Enter size of wings :");
+    scanf("%d",&n);
+    // for upper two half triangles 
+    for(int i=1;i<=n-1;i++){
+        for(int j=1;j<=i;j++){
+            if(j==1 || j==i || i==1 ){
+            printf("*");
+        } else {
+            printf(" ");
+        }
+        }
+        
+          
+        for(int k=0;k<=n-i;k++){
+            printf(" ");
+        }
+
+                   
+
+        for(int k=1;k<=n-i;k++){
+            printf(" ");
+        }
+        for(int j=1;j<=i;j++){
+            if(j==1 || j==i || i==1 ){
+                        printf("*");
+        } else {
+            printf(" ");
+        }
+        }
+        printf("\n");
+    }
+
+       //for bottom two half triangles
+    for(int i=1;i<=n;i++){
+        for(int j=n;j>=i;j--){
+            if( j==n || j==i ){
+            printf("*");
+        } else {
+            printf(" ");
+        }
+        }
+        
+
+         
+        for(int k=0;k<=i-1;k++){
+            printf(" ");
+        }   
+
+
+        for(int k=1;k<=i-1;k++){
+            printf(" ");
+        }
+        for(int j=n;j>=i;j--){
+            if( j==n || j==i ){
+            printf("*");
+        } else {
+            printf(" ");
+        }
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+/*
+output :
+Enter size of wings :10
+*                   *
+**                 **
+* *               * *
+*  *             *  *
+*   *           *   *
+*    *         *    *
+*     *       *     *
+*      *     *      *
+*       *   *       *
+*        * *        *
+*       *   *       *
+*      *     *      *
+*     *       *     *
+*    *         *    *
+*   *           *   *
+*  *             *  *
+* *               * *
+**                 **
+*                   *
+
+*/
