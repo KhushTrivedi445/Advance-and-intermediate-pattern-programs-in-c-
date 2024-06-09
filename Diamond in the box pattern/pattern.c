9c730dfa897e7f53a9f05c84a0d9018174ec51e6
Khush Trivedi <168230450+KhushTrivedi445@users.noreply.github.com>
2024-06-09 16:29:57 +0530

    Add files via upload

---
+++ Diamond in the box pattern/pattern.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+int main () {
+    int n;
+    printf("Enter the size : ");
+    scanf("%d",&n);
+
+    for(int i=1;i<=n;i++){
+        for(int j=n;j>=i;j--){
+              printf("*");
+        }
+        
+    
+
+       for(int k=1;k<=i-1;k++){
+        printf(" ");
+       }
+     
+        for(int k=0;k<=i-2;k++){
+            printf(" ");
+        }
+        for(int j=n;j>=i;j--){
+            printf("*");
+        }
+        printf("\n");
+       }
+        
+
+     for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            printf("*");
+        }
+        
+          for(int k=n;k>=i+1;k--){
+            printf(" ");
+          }
+
+                   
+     
+        for(int k=n;k>=i+1;k--){
+            printf(" ");
+        }
+        for(int j=1;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+     }
+
+      return 0;
+}
+
+
+
+/*
+output :
+Enter the size : 5
+
+**********
+****  ****
+***    ***
+**      **
+*        *
+*        *
+**      **
+***    ***
+****  ****
+**********
+
+*/
