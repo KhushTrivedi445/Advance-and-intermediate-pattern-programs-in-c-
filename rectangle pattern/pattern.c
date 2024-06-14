529e505d1a2519b8ed1a74ec7f7fb4031d66ac23
Khush Trivedi <168230450+KhushTrivedi445@users.noreply.github.com>
2024-06-14 22:28:02 +0530

    Update pattern.c

--- rectangle pattern/pattern.c
+++ rectangle pattern/pattern.c
@@ -1,3 +1,6 @@
+
+
+
 #include<stdio.h>
 
 int main () {
@@ -6,6 +9,7 @@ int main () {
     scanf("%d",&n);
     printf("Enter the number of columns :");
     scanf("%d",&m);
+    This loop for roiws and columns
     for(int i=1;i<=n;i++){
         for(int j=1;j<=m;j++){
             printf("* ");
