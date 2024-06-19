b85a7049b5b0353dafefc169b2adb190de6a4f69
Khush Trivedi <168230450+KhushTrivedi445@users.noreply.github.com>
2024-06-19 19:02:05 +0530

    Update pattern.c

--- Temple like pattern/pattern.c
+++ Temple like pattern/pattern.c
@@ -3,7 +3,7 @@ int main () {
 int n;
 printf("Enter the value of n :");
 scanf("%d",&n);
-
+//This loop is for upper triangle
 for(int i=1 ;i<=n;i++){
         for(int k=1; k<=n-i ; k++){                       //This loop is for spacing
             printf(" ");
@@ -14,7 +14,7 @@ for(int i=1 ;i<=n;i++){
             printf("\n");
     }
 
-
+     //This loop is for the square
   for(int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
             printf("* ");
